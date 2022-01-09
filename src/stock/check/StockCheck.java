ab3176b9a9626f5bbe5b1446310c387dc0417d11
Lexan E Flowers <lexar5657@gmail.com>
2022-01-09 09:34:51 -0600

    Add files via upload

--- src/stock/check/StockCheck.java
+++ src/stock/check/StockCheck.java
@@ -1,15 +1,18 @@
 package stock.check;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author emjai
  */
 public class StockCheck {
 
-    /**
-     * @param args the command line arguments
-     */
+    static ArrayList<Customer> Customers;
+    static ArrayList<order> Orders;
     public static void main(String[] args) {
+        Orders = new ArrayList<>();
+        Customers = new ArrayList<>();
         // TODO code application logic here
     }
     
--- src/stock/check/order.java
+++ src/stock/check/order.java
@@ -1,5 +1,7 @@
 package stock.check;
 
+import java.util.Date;
+
 /**
  *
  * @author emjai
@@ -8,5 +10,7 @@ public class order {
     private int ID;
     private double total,paid,supplies,labour;
     private Customer customer;
+    private String description,item;
+    private Date dateReceived,dateReturned;
 
 }
